9f73c33fc413c9be5e3c76e55c71498d375165ea
nathan barloy <nbarloy@orange.fr>
2020-09-01 22:26:52 +0200

    first bonus

---
+++ Assets/Scripts/Bonus.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Bonus : MonoBehaviour
+{
+    [SerializeField] private float duration = 5f;
+    private float timer;
+    private bool taken;
+
+    private void Start()
+    {
+        timer = duration;
+        taken = false;
+    }
+
+    private void Update()
+    {
+        System.Console.WriteLine(taken);
+        if (taken)
+        {
+            if (timer<=0)
+            {
+                End();
+                Object.Destroy(this.gameObject);
+            }
+            else
+            {
+                timer -= Time.deltaTime;
+            }
+        }
+    }
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        OnPick(collider);
+        taken = true;
+        this.gameObject.GetComponent<Renderer>().enabled = false;
+        this.gameObject.GetComponent<Collider>().enabled = false;
+    }
+
+    public abstract void OnPick(Collider collider);
+    public abstract void End();
+
+}
---
+++ Assets/Scripts/SpeedBonus.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedBonus : Bonus
+{
+    [SerializeField] private float speedFactor = 2.0f;
+    private PlayerController controll;
+    private float resetValue;
+
+    public override void OnPick(Collider collider)
+    {
+        controll = collider.gameObject.GetComponent<PlayerController>();
+        resetValue = controll.speed;
+        controll.speed *= speedFactor;
+    }
+
+    public override void End()
+    {
+        controll.speed = resetValue;
+    }
+
+}
