3be8070a6594fbd724c9ff9cbca372c5552dff8c
XavierIndice <xavier.indice@telecomnancy.net>
2020-09-03 22:39:26 +0200

    Fix shadows and delete useless comments and debug messages

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -6,13 +6,11 @@ using UnityEngine.SceneManagement;
 public class SceneSwitcher : MonoBehaviour
 {
     private Scene scene;
-    //private bool gameLoaded = false;
 
     // Start is called before the first frame update
     void Start()
     {
         scene = SceneManager.GetActiveScene();
-        Debug.Log("Active Scene is '" + scene.name + "'.");
     }
 
     // Update is called once per frame
@@ -20,24 +18,14 @@ public class SceneSwitcher : MonoBehaviour
     {
         if (Input.GetKeyDown("space"))
         {
-            Debug.Log("Space down");
             SceneManager.LoadScene("Game", LoadSceneMode.Single);
-            //gameLoaded = true;
             Debug.Log(SceneManager.sceneCount);
             SceneManager.sceneLoaded += this.loadGame;
         }
-        /*if (gameLoaded)
-        {
-            SceneManager.SetActiveScene(SceneManager.GetSceneAt(1));
-            scene = SceneManager.GetActiveScene();
-            Debug.Log("Active Scene is '" + scene.name + "'.");
-        }*/
     }
 
     void loadGame(Scene scene, LoadSceneMode mode)
     {
-        //SceneManager.SetActiveScene(SceneManager.GetSceneAt(0));
         scene = SceneManager.GetActiveScene();
-        Debug.Log("Active Scene is '" + scene.name + "'.");
     }
 }
