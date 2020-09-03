4816c7bf356f6039bcb383cec18bd538c2f19968
blorpblurp%% <Timothee.Wright@telecomnancy.eu>
2020-09-03 23:29:14 +0200

    bonus ball géante

---
+++ Assets/Scripts/Bonus/GiantBallBonus.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiantBallBonus : Bonus
+{
+    /* Malus de vitesse donné au joueur.
+     * La vitesse est divisée par 'speedfactor'
+     */
+
+    private void Awake()
+    {
+        color = new Color(1f, 1f, 0);
+    }
+
+    private float scaleFactor = 2.0f;
+
+    public override void OnPick(Collider collider)
+    {
+		Ball.instance.transform.localScale = Ball.instance.transform.localScale * scaleFactor;
+    }
+
+    public override void End()
+    {
+		Ball.instance.transform.localScale = Ball.instance.transform.localScale / scaleFactor;
+
+	}
+
+}
