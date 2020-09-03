64f3d49c836e3d4da6e92f2ddb1f15bb2246096b
XavierIndice <xavier.indice@telecomnancy.net>
2020-09-03 22:21:47 +0200

    Add title screen (with transition to game scene)

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    private Scene scene;
+    //private bool gameLoaded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+        Debug.Log("Active Scene is '" + scene.name + "'.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("Space down");
+            SceneManager.LoadScene("Game", LoadSceneMode.Single);
+            //gameLoaded = true;
+            Debug.Log(SceneManager.sceneCount);
+            SceneManager.sceneLoaded += this.loadGame;
+        }
+        /*if (gameLoaded)
+        {
+            SceneManager.SetActiveScene(SceneManager.GetSceneAt(1));
+            scene = SceneManager.GetActiveScene();
+            Debug.Log("Active Scene is '" + scene.name + "'.");
+        }*/
+    }
+
+    void loadGame(Scene scene, LoadSceneMode mode)
+    {
+        //SceneManager.SetActiveScene(SceneManager.GetSceneAt(0));
+        scene = SceneManager.GetActiveScene();
+        Debug.Log("Active Scene is '" + scene.name + "'.");
+    }
+}
---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer = timer + Time.deltaTime;
+        if (timer >= 0.7)
+        {
+            GetComponent<TextMesh>().text = "Appuyez sur Espace";
+        }
+        if (timer >= 1.4)
+        {
+            GetComponent<TextMesh>().text = "";
+            timer = 0;
+        }
+    }
+}
