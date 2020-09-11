8b9c4e1cfca3802e9a1596a76e31a1818eb43e87
HULOT Stephane <stephane.hulot@telecomnancy.eu>
2020-09-11 18:37:17 +0200

    Ajout du bonus de mur

---
+++ Assets/Scripts/Bonus/WallBonus.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallBonus : Bonus
+{
+
+    public GameObject wallPrefab;
+    private GameObject myWall;
+
+    public override void OnPick(Collider collider)
+    {
+        myWall = Instantiate(wallPrefab);
+    }
+
+    public override void End()
+    {
+        Destroy(myWall);
+    }
+
+}
---
+++ Assets/Scripts/RandomWalls.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomWalls : MonoBehaviour
+{
+    void Start()
+    {
+        if (transform.childCount < 3)
+            return;
+
+        transform.GetChild(0).Translate(Vector3.right * Random.Range(-30, 30) / 10f);
+        transform.GetChild(1).Translate(Vector3.right * Random.Range(-30, 30) / 10f);
+        transform.GetChild(2).Translate(Vector3.forward * Random.Range(-30, 30) / 10f);
+        transform.GetChild(3).Translate(Vector3.forward * Random.Range(-30, 30) / 10f);
+    }
+}
