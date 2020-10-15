5e3fad21076519e71fa08181005e31de96a9fb30
HULOT Stephane <stephane.hulot@telecomnancy.eu>
2020-10-15 15:48:01 +0200

    Navigation du menu au clavier

--- Assets/Scripts/TeamWheel.cs
+++ Assets/Scripts/TeamWheel.cs
@@ -56,51 +56,64 @@ public class TeamWheel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Q))
+            PreviousTeam(0);
+        if (Input.GetKeyDown(KeyCode.D))
+            NextTeam(0);
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            PreviousTeam(1);
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            NextTeam(1);
+        if (Input.GetKeyDown(KeyCode.Space))
+            StartGame();
         /*
-        if (SceneManager.GetActiveScene().name == "TeamSelector")
+        else if (Input.GetKeyDown(d) || (Input.GetMouseButtonUp(0) && coll.name == "Right" && gameObject.name == coll.gameObject.transform.parent.parent.name))
         {
+            
+            if (SceneManager.GetActiveScene().name == "TeamSelector")
+            {
 
-            // Check mouse controls
-            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit))
-            {
-                coll = hit.collider;
-            }
+                // Check mouse controls
+                ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit))
+                {
+                    coll = hit.collider;
+                }
 
-                if (timer >= 0.1)
-            {
-                gameObject.transform.GetChild(1).Find(selected).GetComponent<MeshRenderer>().material = mat1;
-                timer = 0;
-            }
-            else if (timer > 0)
-            {
-                timer = timer + Time.deltaTime;
-            }
-            else if (Input.GetKeyDown(q) || (Input.GetMouseButtonUp(0) && coll.name == "Left" && gameObject.name == coll.gameObject.transform.parent.parent.name))
-            {
-                i -= 1;
-                if (i < 0) i = teams.Count - 1;
-                this.gameObject.transform.Find("Team_Logo").GetComponent<MeshRenderer>().material.SetTexture("_MainTex", Resources.Load<Texture>(string.Concat("Logos/", teams[i].getFileId())));
-                this.gameObject.transform.GetChild(1).Find("Name").GetComponent<TextMesh>().text = teams[i].getName();
-                updateLogos();
-                gameObject.transform.GetChild(1).Find("Left").GetComponent<MeshRenderer>().material = mat2;
-                selected = "Left";
-                timer = timer + Time.deltaTime;
-            }
-            else if (Input.GetKeyDown(d) || (Input.GetMouseButtonUp(0) && coll.name == "Right" && gameObject.name == coll.gameObject.transform.parent.parent.name))
-            {
-                i += 1;
-                if (i >= teams.Count) i = 0;
-                this.gameObject.transform.Find("Team_Logo").GetComponent<MeshRenderer>().material.SetTexture("_MainTex", Resources.Load<Texture>(string.Concat("Logos/", teams[i].getFileId())));
-                this.gameObject.transform.GetChild(1).Find("Name").GetComponent<TextMesh>().text = teams[i].getName();
-                updateLogos();
-                gameObject.transform.GetChild(1).Find("Right").GetComponent<MeshRenderer>().material = mat2;
-                selected = "Right";
-                timer = timer + Time.deltaTime;
-            }
+                    if (timer >= 0.1)
+                {
+                    gameObject.transform.GetChild(1).Find(selected).GetComponent<MeshRenderer>().material = mat1;
+                    timer = 0;
+                }
+                else if (timer > 0)
+                {
+                    timer = timer + Time.deltaTime;
+                }
+                else if (Input.GetKeyDown(q) || (Input.GetMouseButtonUp(0) && coll.name == "Left" && gameObject.name == coll.gameObject.transform.parent.parent.name))
+                {
+                    i -= 1;
+                    if (i < 0) i = teams.Count - 1;
+                    this.gameObject.transform.Find("Team_Logo").GetComponent<MeshRenderer>().material.SetTexture("_MainTex", Resources.Load<Texture>(string.Concat("Logos/", teams[i].getFileId())));
+                    this.gameObject.transform.GetChild(1).Find("Name").GetComponent<TextMesh>().text = teams[i].getName();
+                    updateLogos();
+                    gameObject.transform.GetChild(1).Find("Left").GetComponent<MeshRenderer>().material = mat2;
+                    selected = "Left";
+                    timer = timer + Time.deltaTime;
+                }
+                else if (Input.GetKeyDown(d) || (Input.GetMouseButtonUp(0) && coll.name == "Right" && gameObject.name == coll.gameObject.transform.parent.parent.name))
+                {
+                    i += 1;
+                    if (i >= teams.Count) i = 0;
+                    this.gameObject.transform.Find("Team_Logo").GetComponent<MeshRenderer>().material.SetTexture("_MainTex", Resources.Load<Texture>(string.Concat("Logos/", teams[i].getFileId())));
+                    this.gameObject.transform.GetChild(1).Find("Name").GetComponent<TextMesh>().text = teams[i].getName();
+                    updateLogos();
+                    gameObject.transform.GetChild(1).Find("Right").GetComponent<MeshRenderer>().material = mat2;
+                    selected = "Right";
+                    timer = timer + Time.deltaTime;
+                }
 
-        }
-        */
+            }
+            */
     }
 
     
