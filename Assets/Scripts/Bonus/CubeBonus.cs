0230cfed1c67db85f4107f6c74e6d2eca5eae467
Alex2740 <alex2740.mc@gmail.com>
2020-10-15 16:00:03 +0200

    Added Bomb Slide Cude Bonus

--- Assets/Scripts/Bonus/CubeBonus.cs
+++ Assets/Scripts/Bonus/CubeBonus.cs
@@ -4,23 +4,18 @@ using UnityEngine;
 
 public class CubeBonus : Bonus
 {
-    /*
-     * La balle est transformée en cube
-     */
-
-    public Mesh cubeMesh; //cube par défaut de Unity
-    public Mesh sphereMesh; //uv_sphere : modèle de base de la balle
+    public Mesh cube;
+    public Mesh sphere;
 
     public override void OnPick(Collider collider)
     {
-        Ball.instance.GetComponent<MeshFilter>().mesh = cubeMesh;
-        Ball.instance.gameObject.AddComponent<BoxCollider>();
+        Ball.instance.GetComponent<MeshFilter>().mesh = cube;
+        Ball.instance.GetComponent<BoxCollider>().enabled = true;
     }
 
     public override void End()
     {
-        Ball.instance.GetComponent<MeshFilter>().mesh = sphereMesh;
-        Destroy(Ball.instance.GetComponent<BoxCollider>());
+        Ball.instance.GetComponent<MeshFilter>().mesh = sphere;
+        Ball.instance.GetComponent<BoxCollider>().enabled = false;
     }
-
 }
